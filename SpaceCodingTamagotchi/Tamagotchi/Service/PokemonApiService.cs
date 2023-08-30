8ee56a31d0796388562dfeb16bed4a592e12257f
Gi Bordignon <contateagiulia@gmail.com>
2023-08-29 22:08:06 -0300

    Adiciona tratamento de exceções nas requisições da API
    - Implementa blocos try-catch em GetPokemonDetailsAsync para lidar com possíveis exceções durante as solicitações à API.
    - Adiciona mensagens de erro específicas para diferentes tipos de exceções.
    - Retorna nulo se a solicitação falhar por qualquer motivo."

--- SpaceCodingTamagotchi/Tamagotchi/Service/PokemonApiService.cs
+++ SpaceCodingTamagotchi/Tamagotchi/Service/PokemonApiService.cs
@@ -13,26 +13,61 @@ namespace Tamagotchi.Service
     {
         public List<PokemonResult> ObterEspeciesDisponiveis()
         {
-            // Obter a lista de espécies de Pokémons
+            try
+            {
+                // Obter a lista de espécies de Pokémons
 
-            var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
-            var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
+                var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
+                var request = new RestRequest(Method.GET);
+                IRestResponse response = client.Execute(request);
+                if (response.IsSuccessful)
+                {
+                    var pokemonEspeciesResposta = JsonConvert.DeserializeObject<PokemonSpeciesResult>(response.Content);
 
-            var pokemonEspeciesResposta = JsonConvert.DeserializeObject<PokemonSpeciesResult>(response.Content);
+                    return pokemonEspeciesResposta.Results;
+                }
+                Console.WriteLine($"Erro ao obter detalhes do Pokémon: {response.Content}");
+                return null;
 
-            return pokemonEspeciesResposta.Results;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Erro de solicitação: {e.Message}");
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+                return null;
+            }
         }
 
         public PokemonDetailsResult ObterDetalhesDaEspecie(PokemonResult especie)
         {
-            // Obter as características do Pokémon escolhido
-            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{especie.Name}");
-            var request = new RestRequest(Method.GET);
-            var response = client.Execute(request);
+            try
+            {
+                // Obter as características do Pokémon escolhido
+                var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{especie.Name}");
+                var request = new RestRequest(Method.GET);
+                var response = client.Execute(request);
+                if (response.IsSuccessful)
+                {
+                    return JsonConvert.DeserializeObject<PokemonDetailsResult>(response.Content);
+                }
+                Console.WriteLine($"Erro ao obter detalhes do Pokémon: {response.Content}");
+                return null;
 
-            return JsonConvert.DeserializeObject<PokemonDetailsResult>(response.Content);
-
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Erro de solicitação: {e.Message}");
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+                return null;
+            }
         }
     }
 }
